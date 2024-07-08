1098da765065aad86c18f007cc743b4821998385
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-07-08 20:15:27 +0200

    feat(S2Automate.kt): add support for withResultAsAction property in S2Automate class to track if transitions have results refactor(SsmExtention.kt): update toSsmTransitions and toSsmTransition functions to use withResultAsAction property refactor(SsmAutomatePersister.kt): introduce withEventAsAction variable to handle event as action based on withResultAsAction property feat(OrderBookDeciderEventImpl.kt): implement OrderBookDecider interface with order book creation, update, publish, and close decisions feat(OrderBookAutomateConfig.kt): rename automate function to automate2 and update the name property in the s2 block to include a unique identifier
    The changes were made to enhance the functionality of the S2Automate, SsmExtention, SsmAutomatePersister, OrderBookDeciderEventImpl, and OrderBookAutomateConfig classes by introducing a new property to track if transitions have results, updating functions to utilize this property, and improving naming conventions for better clarity and organization.
    
    feat(orderbook-storing): add OrderBookAutomateEventConfig and OrderBookStoringDeciderEventImplTest classes
    The OrderBookAutomateEventConfig class is added to configure the automation of order book events in the storing application. This class sets up the automation process for creating, updating, publishing, and closing order books. It also specifies the chaincode URI and signer agent for the automation.
    
    The OrderBookStoringDeciderEventImplTest class is added to test the functionality of the order book storing decider events. It includes tests for creating order books, updating them, publishing them, and closing them. Additionally, it tests the flow of events to build entities and verifies the status of the order books after each operation.

--- s2-automate/s2-automate-dsl/src/commonMain/kotlin/s2/dsl/automate/ssm/SsmExtention.kt
+++ s2-automate/s2-automate-dsl/src/commonMain/kotlin/s2/dsl/automate/ssm/SsmExtention.kt
@@ -5,24 +5,26 @@ import s2.dsl.automate.S2Transition
 import ssm.chaincode.dsl.model.Ssm
 import ssm.chaincode.dsl.model.SsmTransition
 
-fun S2Automate.toSsm(permissive: Boolean = false) = Ssm(
-	name = this.name,
-	transitions = if(permissive) {
-		this.transitions.toSsmTransitions(0, 0)
-	} else {
-		this.transitions.toSsmTransitions()
-	}
-)
-
-fun Array<out S2Transition>.toSsmTransitions(
-	from: Int? = null, to: Int? = null
-) = filter { it.from != null }. map {
-	it.toSsmTransition(from, to)
+fun S2Automate.toSsm(permissive: Boolean = false): Ssm {
+	return Ssm(
+		name = this.name,
+		transitions = if (permissive) {
+			this.transitions.toSsmTransitions(0, 0, withResultAsAction)
+		} else {
+			this.transitions.toSsmTransitions(withResultAsAction = withResultAsAction)
+		}
+	)
 }
 
-fun S2Transition.toSsmTransition(from: Int? = null, to: Int? = null) = SsmTransition(
+fun Array<out S2Transition>.toSsmTransitions(
+	from: Int? = null, to: Int? = null, withResultAsAction: Boolean
+) = filter { it.from != null }. map {
+	it.toSsmTransition(from, to, withResultAsAction)
+}
+
+fun S2Transition.toSsmTransition(from: Int? = null, to: Int? = null, withResultAsAction: Boolean) = SsmTransition(
 	from = from ?: this.from!!.position,
 	to = to ?: this.to.position,
 	role = this.role.name,
-	action = this.result?.name ?: this.action.name
+	action = this.result?.name?.takeIf { withResultAsAction } ?: this.action.name
 )
