21614d7e11c1e57f173a3f2c18882b7ef780788e
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-08-22 22:26:42 +0200

    refactor(did-domain): unify DIDFunctionClient implementation and remove platform-specific code
    Replace platform-specific implementations of DIDFunctionClient with a single, unified implementation. This simplifies the codebase by removing redundant code and centralizing the client creation logic. The new implementation uses F2ClientBuilder to create the client based on a URL, making it more flexible and easier to maintain.

--- sample/did-sample/did-domain/src/commonMain/kotlin/s2/sample/did/domain/client/DIDFunctionClient.kt
+++ sample/did-sample/did-domain/src/commonMain/kotlin/s2/sample/did/domain/client/DIDFunctionClient.kt
@@ -2,8 +2,8 @@ package s2.sample.did.domain.client
 
 import f2.client.F2Client
 import f2.client.function
-import f2.client.ktor.Protocol
-import f2.dsl.fnc.F2Supplier
+import f2.client.ktor.F2ClientBuilder
+import f2.dsl.fnc.F2SupplierSingle
 import kotlin.js.JsExport
 import kotlin.js.JsName
 import s2.sample.did.domain.DidAggregate
@@ -12,11 +12,19 @@ import s2.sample.did.domain.features.DidCreateCommandFunction
 import s2.sample.did.domain.features.DidRevokeCommandFunction
 import s2.sample.did.domain.features.DidRevokePublicKeyCommandFunction
 
-expect fun didClient(protocol: Protocol, host: String, port: Int, path: String? = null): F2Supplier<DIDFunctionClient>
+fun didClient(
+	urlBase: String,
+): F2SupplierSingle<DIDFunctionClient> {
+	return F2SupplierSingle {
+		val s2Client = F2ClientBuilder.get(urlBase)
+		DIDFunctionClient(s2Client)
+	}
+}
+
 
 @JsName("DIDFunctionClient")
 @JsExport
-open class DIDFunctionClient constructor(private val client: F2Client) : DidAggregate {
+open class DIDFunctionClient(private val client: F2Client) : DidAggregate {
 	override fun createDid(): DidCreateCommandFunction = client.function("createDid")
 	override fun addPublicKey(): DidAddPublicKeyCommandFunction = client.function("addPublicKey")
 	override fun revokePublicKey(): DidRevokeCommandFunction = client.function("revokePublicKey")
