2734bd19809eaabdf2463c4df316c736d3c346e6
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-10-18 16:26:31 +0200

    feat(storing-data): add support for flow-based persistence in configurer adapters
    Introduce `AutomatePersisterFlow` to both `S2SpringDataConfigurerAdapter`
    and `S2SpringDataSuspendConfigurerAdapter` to support flow-based
    persistence. This change allows for more flexible and efficient data
    handling by leveraging flow-based operations, which are particularly
    useful in reactive and coroutine-based environments. The new methods
    `aggregateRepositoryFlow` return instances of `SpringDataAutomatePersisterFlow`
    and `SpringDataAutomateCoroutinePersisterFlow`, enabling the use of
    flow-based persistence strategies.

--- s2-spring/storing/s2-spring-boot-starter-storing-data/src/main/kotlin/s2/spring/automate/data/S2SpringDataConfigurerAdapter.kt
+++ s2-spring/storing/s2-spring-boot-starter-storing-data/src/main/kotlin/s2/spring/automate/data/S2SpringDataConfigurerAdapter.kt
@@ -2,6 +2,7 @@ package s2.spring.automate.data
 
 import org.springframework.data.repository.CrudRepository
 import s2.automate.core.persist.AutomatePersister
+import s2.automate.core.persist.AutomatePersisterFlow
 import s2.dsl.automate.Evt
 import s2.dsl.automate.S2Automate
 import s2.dsl.automate.S2State
@@ -9,6 +10,7 @@ import s2.dsl.automate.model.WithS2Id
 import s2.dsl.automate.model.WithS2State
 import s2.spring.automate.S2ConfigurerAdapter
 import s2.spring.automate.data.persister.SpringDataAutomatePersister
+import s2.spring.automate.data.persister.SpringDataAutomatePersisterFlow
 import s2.spring.automate.executor.S2AutomateExecutorSpring
 
 abstract class S2SpringDataConfigurerAdapter<STATE, ID, ENTITY, AGGREGATE>(
@@ -24,4 +26,11 @@ AGGREGATE : S2AutomateExecutorSpring<STATE, ID, ENTITY> {
 			aggregateRepository
 		)
 	}
+
+	override fun aggregateRepositoryFlow(): AutomatePersisterFlow<STATE, ID, ENTITY, Evt, S2Automate> {
+		return SpringDataAutomatePersisterFlow(
+			aggregateRepository
+		)
+	}
+
 }
